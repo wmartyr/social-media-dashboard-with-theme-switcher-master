4232598838e0c0226b56faa028dca16768e3c209
Woodrow Martyr <woodrow.martyr@gmail.com>
2022-08-22 18:16:01 +0930

    Code dark theme hover effects

--- scripts/script.js
+++ scripts/script.js
@@ -10,6 +10,7 @@ function changeTheme() {
     root.style.setProperty("--color-theme-card-background", "var(--color-dark-theme-blue3)");
     root.style.setProperty("--color-theme-text1", "var(--color-dark-theme-blue4)");
     root.style.setProperty("--color-theme-text2", "white");
+    root.style.setProperty("--color-card-hover", "var(--color-card-hover-dark)");
     sliderRound.classList.replace("slider-round-light", "slider-round-dark");
     overviewTitle.classList.replace("overview-title-light", "overview-title-dark");
   } else {
@@ -18,6 +19,7 @@ function changeTheme() {
     root.style.setProperty("--color-theme-card-background", "var(--color-light-theme-blue2)");
     root.style.setProperty("--color-theme-text1", "var(--color-light-theme-blue3)");
     root.style.setProperty("--color-theme-text2", "var(--color-light-theme-blue4)");
+    root.style.setProperty("--color-card-hover", "var(--color-card-hover-light)");
     sliderRound.classList.replace("slider-round-dark", "slider-round-light");
     overviewTitle.classList.replace("overview-title-dark", "overview-title-light");
   }
